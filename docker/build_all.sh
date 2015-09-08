ce3ea32204dd462ea0db9f84850677d17d42a682
Michael Laraia <micheal.laraia@gmail.com>
2015-09-08 17:49:00 -0400

    prevent code portion from using cache

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -2,4 +2,4 @@
 cd ~/Documents/Yale/server/docker
 docker build -t geneseq/install:1.0 install && docker tag -f geneseq/install:1.0 geneseq/install:latest && \
 docker build -t geneseq/configure:1.0 configure && docker tag -f geneseq/configure:1.0 geneseq/configure:latest && \
-docker build -t geneseq/code:1.0 code && docker tag -f geneseq/code:1.0 geneseq/code:latest
+docker build --no-cache -t geneseq/code:1.0 code && docker tag -f geneseq/code:1.0 geneseq/code:latest && docker tag -f geneseq/code:1.0 miclaraia/geneseq:latest
