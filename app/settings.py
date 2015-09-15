7f77fef7a18e31e8f9a7bbd24e42fb2740f03a1c
Michael Laraia <micheal.laraia@gmail.com>
2015-09-15 16:06:00 -0400

    removed class

--- app/settings.py
+++ app/settings.py
@@ -2,141 +2,149 @@ import yaml
 import os
 import logging
 import copy
+import app.mongo_pipe
 
 logger = logging.getLogger(__name__)
+path = os.path.dirname(os.path.realpath(__file__))
+
+config = None
 
 
-class Settings(object):
-    """handles access to settings stored in settings.yaml"""
+def init():
+    global config
+    global pipe
 
-    def __init__(self):
-        self.path = os.path.dirname(os.path.realpath(__file__))
-        self.settings = self.loadConfig()
+    config = loadConfig()
+    pipe = app.mongo_pipe.Pipe()
+    logger.debug(getConfig())
 
-    def getConfig(self):
-        """gets config dictionary
-        Returns:
-            dict: config dictionary
-        """
-        return copy.deepcopy(self.settings)
 
-    def setCurrentCount(self, count):
-        sliders = self.settings['table_sliders']
-        self.settings['count'] = count
-        for slider in sliders:
-            if slider['column'] == 'limit':
-                logger.debug('found limit slider, changing max to %s' % count)
-                slider['max'] = count
+def getConfig():
+    """gets config dictionary
+    Returns:
+        dict: config dictionary
+    """
+    return copy.deepcopy(config)
 
-    def loadConfig(self):
-        """Loads config from yaml file"""
-        logger.debug('loading config from file')
-        f = open("%s/conf/settings.yaml" % self.path, 'r')
-        raw = f.read()
-        f.close()
-        return yaml.load(raw)
 
-    def translate(self, key):
-        """translates python var names to mysql column names
+def setCurrentCount(count):
+    sliders = config['table_sliders']
+    config['count'] = count
+    for slider in sliders:
+        if slider['column'] == 'limit':
+            logger.debug('found limit slider, changing max to %s' % count)
+            slider['max'] = count
 
-        Args:
-            key (str): key to search in config
-        Returns:
-            str: translated key
-            None: returns key if no match found
-        """
-        logger.info('translating, key: %s' % key)
-        if key in self.settings['translate']:
-            colName = self.settings['translate'][key]
-            logger.debug('key found, translated to: %s' % colName)
-            return colName
-        else:
-            logger.debug('key not found')
-            return key
 
-    def translate_readable(self, key):
-        """translates variables names to their human readable form
+def loadConfig():
+    """Loads config from yaml file"""
+    logger.debug('loading config from file')
+    f = open("%s/conf/settings.yaml" % path, 'r')
+    raw = f.read()
+    f.close()
+    return yaml.load(raw)
 
-        Args:
-            key (str): key to search in config
-        Returns:
-            str: translated key
-            None: returns key if no match found
-        """
-        logger.info('translating key %s' % key)
-        if key in self.settings['readable']:
-            name = self.settings['readable'][key]
-            logger.debug('key found, translated to %s' % name)
-            return name
-        else:
-            logger.debug('key not found')
-            return key
+def translate(key):
+    """translates python var names to mysql column names
 
-    def getColumnNames(self):
-        """Gets the visible and variable names of the columns
-        displayed in the html data table
+    Args:
+        key (str): key to search in config
+    Returns:
+        str: translated key
+        None: returns key if no match found
+    """
+    logger.info('translating, key: %s' % key)
+    if key in config['translate']:
+        colName = config['translate'][key]
+        logger.debug('key found, translated to: %s' % colName)
+        return colName
+    else:
+        logger.debug('key not found')
+        return key
 
-        Returns:
-            list: data on a column
-                list:
-                    [0] Human readable name
-                    [1] Variable name in condensed
-        """
-        return copy.deepcopy(self.settings['table_columns'])
+def translate_readable(key):
+    """translates variables names to their human readable form
 
-    def getDefaultLimit(self):
-        """finds default limit set in limit-slider config
+    Args:
+        key (str): key to search in config
+    Returns:
+        str: translated key
+        None: returns key if no match found
+    """
+    logger.info('translating key %s' % key)
+    if key in config['readable']:
+        name = config['readable'][key]
+        logger.debug('key found, translated to %s' % name)
+        return name
+    else:
+        logger.debug('key not found')
+        return key
 
-        Returns:
-            int: default query limit
-        """
-        for item in self.settings['table_sliders']:
-                if item['column'] == 'limit':
-                    return copy.deepcopy(item['init'])
+def getColumnNames():
+    """Gets the visible and variable names of the columns
+    displayed in the html data table
 
-    # returns set of SELECT column names in
-    # mysql syntax form
-    def parseIDs(self, ids):
-        """returns set of SELECT column names in
-        mysql syntax
+    Returns:
+        list: data on a column
+            list:
+                [0] Human readable name
+                [1] Variable name in condensed
+    """
+    return copy.deepcopy(config['table_columns'])
 
-        Args:
-            ids: (list, str)
-                if type is list, joins list together and returns
-                if type is str, searches for match in config
-        Returns:
-            str: mysql columns for SELECT
-        """
-        if ids is None:
-            return '*'
-        if type(ids) is list:
-            return ','.join(ids)
-        elif ids in self.settings['SELECT']:
-            return (','.join(self.settings['SELECT'][ids]))
-        else:
-            return copy.deepcopy(ids)
+def getDefaultLimit():
+    """finds default limit set in limit-slider config
 
-    def getTableSliders(self):
-        """gets list of sliders from configuration
-        Returns:
-            list: list of sliders and their config data
-                -
-                    name:     (str) Name of slider for viewing
-                    column:   (str) var name associated with slider
-                    scale:    (str) linear or logarithmic scale used for slider
-                    min:      (int) min value of slider
-                    max:      (int) max value of slider
-                    init:(int,list) initial value/range of slider
-        """
-        sliders = list()
-        input = self.settings['table_input']
-        for item in input:
-            if item['type'] == 'slider':
-                sliders.append(item)
-        return copy.deepcopy(sliders)
+    Returns:
+        int: default query limit
+    """
+    for item in config['table_sliders']:
+            if item['column'] == 'limit':
+                return copy.deepcopy(item['init'])
 
-    def getTableFilters(self):
-        return copy.deepcopy(self.settings['table_input'])
+# returns set of SELECT column names in
+# mysql syntax form
+def parseIDs(ids):
+    """returns set of SELECT column names in
+    mysql syntax
 
-    def getOrder(self, name):
-        return copy.deepcopy(self.settings['order'][name])
+    Args:
+        ids: (list, str)
+            if type is list, joins list together and returns
+            if type is str, searches for match in config
+    Returns:
+        str: mysql columns for SELECT
+    """
+    if ids is None:
+        return '*'
+    if type(ids) is list:
+        return ','.join(ids)
+    elif ids in config['SELECT']:
+        return (','.join(config['SELECT'][ids]))
+    else:
+        return copy.deepcopy(ids)
+
+def getTableSliders():
+    """gets list of sliders from configuration
+    Returns:
+        list: list of sliders and their config data
+            -
+                name:     (str) Name of slider for viewing
+                column:   (str) var name associated with slider
+                scale:    (str) linear or logarithmic scale used for slider
+                min:      (int) min value of slider
+                max:      (int) max value of slider
+                init:(int,list) initial value/range of slider
+    """
+    sliders = list()
+    input = config['table_input']
+    for item in input:
+        if item['type'] == 'slider':
+            sliders.append(item)
+    return copy.deepcopy(sliders)
+
+def getTableFilters():
+    return copy.deepcopy(config['table_input'])
+
+def getOrder(name):
+    return copy.deepcopy(config['order'][name])
